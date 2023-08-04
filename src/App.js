7ee2ebbe6644cef5b450a2f0469a498ade1cc6fe
yongsung <mirr001003@hanyang.ac.kr>
2023-08-05 01:10:50 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Videos from  './pages/Videos';
+import Root from './pages/Root';
+import VideoDetail from './pages/VideoDetail';
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Root/>,
+    errorElement: <NotFound/>,
+    children: [
+      { index: true, element: <Home/>},
+      { path: '/videos', element: <Videos/>},
+      { path: '/videos/:videoId', element: <VideoDetail/>}
+    ]
+  },
+  {
+    path:'/videos',
+    element:<Videos/>
+  }
+])
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
